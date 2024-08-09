92d7cb59839778e2e04913124669ee418ba84634
Noelia <Noecordero01@gmail.com>
2024-08-09 13:55:43 -0600

    cambio ruta

--- src/api/clientes.api.js
+++ src/api/clientes.api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const insertarCliente = async (clienteNuevo) => {
     try {
-        const response = await axios.post('http://localhost:4000/insetarCliente', clienteNuevo);
+        const response = await axios.post('http://localhost:4000/insertarCliente', clienteNuevo);
         return response.data; 
     } catch (error) {
         console.error('Error al insertar cliente:', error);
